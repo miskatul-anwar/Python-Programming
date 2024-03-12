78c97b4ba0ab1feea9a432f45716f7a09d1eb8eb
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-12 05:04:43 +0000

    added

---
+++ sets.py
@@ -0,0 +1,9 @@
+set = {1, 2, 3, "set", "this"}
+print(set)
+# sets are mutable but the elements are not mutable
+set.add("My name is Miskat")
+print(set)
+set.remove(1)
+print(set)
+set.clear()  # empties the set
+set.pop()  # shows a random value
---
+++ sets_operation.py
@@ -0,0 +1,6 @@
+set1 = {1, 2, 3, 4}
+set2 = {1, 2, 6, 21, 8}
+result1 = set1.union(set2)
+result2 = set1.intersection(set2)
+print(result1)
+print(result2)
