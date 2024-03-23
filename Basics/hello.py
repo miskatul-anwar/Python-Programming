a9a37f0e21e3fa1f0c479595bd9a9d7f608765ed
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-23 13:37:58 +0000

    Sat Mar 23 01:37:58 PM UTC 2024

--- Basics/hello.py
+++ Basics/hello.py
@@ -2,6 +2,7 @@ tiffin = 200
 stipend = 300
 spending = 700
 fox = 6700
-print((tiffin + stipend -spending)* 12)
-a = "I Love You\n"
-print(100* a)
+print((tiffin + stipend - spending) * 12)
+print("hi")
+print(fox * 12)
+print("Here's the documents you'll need so far")
