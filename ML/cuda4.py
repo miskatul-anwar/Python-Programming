be93aa12e7c6ff56e939ca37fcb131ca52d74169
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-24 02:29:09 +0000

    Sun Mar 24 02:29:08 AM UTC 2024

--- ML/cuda4.py
+++ ML/cuda4.py
@@ -6,6 +6,12 @@ if torch.cuda.is_available():
 else:
     device = torch.device("cpu")
 print(device)
+
+# Create tensors and move them to the selected device
+a = torch.arange(100000, device=device) * 10
+
+# Synchronize CUDA operations
 torch.cuda.synchronize()
-for i in range(100000):
-    print(i * 10)
+
+# Print the tensors
+print(a)
