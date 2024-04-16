d405f816a9dc1e591f424e8876956457798edb98
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-04-16 20:57:54 +0600

    Tue Apr 16 08:57:54 PM +06 2024

--- MatPlotLib/basic_graph.py
+++ MatPlotLib/basic_graph.py
@@ -1,5 +1,13 @@
 import matplotlib.pyplot as plt
 
 x, y = [1, 2, 3], [4, 5, 6]
+plt.title(
+    "A 2D graph",
+    fontdict={
+        "fontname": "Cambria",
+    },
+)
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
 plt.plot(x, y)
 plt.show()
