c5be137b75f2a4c61a4a8f6b85ef59f8f68f4065
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-09-18 11:08:56 +0600

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   __pycache__/BasicAnimation.cpython-312.pyc
    	modified:   __pycache__/critical_points.cpython-312.pyc
    	modified:   critical_points.py
    	new file:   media/videos/critical_points/480p15/partial_movie_files/Crit/partial_movie_file_list.txt

--- Manim/Anim/critical_points.py
+++ Manim/Anim/critical_points.py
@@ -1,13 +1,16 @@
 import random as rd
 from manim import *
 from manim.utils.color.BS381 import RED_OXIDE
-from manim.utils.color.X11 import GRAY60, PALETURQUOISE4
+from manim.utils.color.X11 import BROWN2, GRAY60, LAVENDERBLUSH3, PALETURQUOISE4
 from manim.utils.color.XKCD import (
     BLUEPURPLE,
+    BLUEVIOLET,
     BROWNGREEN,
     GREYBROWN,
     MOSSGREEN,
     NEONGREEN,
+    ORANGEBROWN,
+    ORANGEISH,
     ORANGEYYELLOW,
     PALEBLUE,
     PINEGREEN,
@@ -19,32 +22,45 @@ class Crit(Scene):
     def construct(self):
         c = Circle(color=GREYBROWN, fill_opacity=0.5)
         self.play(FadeIn(c))
-        cross_color = [RED, GREEN, BLUE, YELLOW]
-        for d in [(0, 0, 0), UP, RIGHT, UR, DR, DOWN, DL, LEFT, UL]:
-            size = len(cross_color)
-            index = rd.randint(0, size - 1)
+        colors = [
+            RED,
+            GREEN,
+            BLUE,
+            YELLOW,
+            RED_OXIDE,
+            ORANGEBROWN,
+            BLUEVIOLET,
+            MOSSGREEN,
+            BROWN2,
+            NEONGREEN,
+            ORANGEISH,
+            LAVENDERBLUSH3,
+        ]
+
+        directions = [
+            (0, 0, 0),
+            UP,
+            RIGHT,
+            UR,
+            DR,
+            DOWN,
+            DL,
+            LEFT,
+            UL,
+        ]
+        for d in directions:
+            size = len(colors) - 1
+            index = rd.randint(0, size)
             self.play(
                 Create(
                     (
                         Cross(scale_factor=0.2).move_to(c.get_critical_point(d))
-                    ).set_color(cross_color[index])
+                    ).set_color(colors[index])
                 )
             )
-        color = [
-            RED_OXIDE,
-            ORANGEYYELLOW,
-            PINEGREEN,
-            MOSSGREEN,
-            BLUEPURPLE,
-            GRAY60,
-            NEONGREEN,
-            PALEBLUE,
-            MAROON_E,
-            BROWNGREEN,
-        ]
         for perc in range(15, 51, 5):
-            size = len(color)
-            index = rd.randint(0, size - 1)
+            size = len(colors) - 1
+            index = rd.randint(0, size)
             self.play(
-                Create(Circle(radius=perc * Percent(X_AXIS)).set_color(color[index]))
+                Create(Circle(radius=perc * Percent(X_AXIS)).set_color(colors[index]))
             )
